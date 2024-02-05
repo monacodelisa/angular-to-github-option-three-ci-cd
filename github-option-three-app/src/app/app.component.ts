b382d7d098353b16bd1def6853c4ed6ab7de0cb1
Esther White <monacodelisa@gmail.com>
2024-02-05 16:14:07 +0200

    couple of ajustments to the layout

--- github-option-three-app/src/app/app.component.ts
+++ github-option-three-app/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
