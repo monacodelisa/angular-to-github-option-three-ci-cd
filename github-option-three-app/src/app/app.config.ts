981fd4baacbad6acd7ed778a8d319f2a1ebcd46f
Esther White <monacodelisa@gmail.com>
2024-02-07 23:30:33 +0200

    animations, for the 'browseranimationsmodule' error'

--- github-option-three-app/src/app/app.config.ts
+++ github-option-three-app/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from "@angular/platform-browser/animations";
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),provideAnimations(),]
 };
--- github-option-three-app/src/app/components/about/about.component.ts
+++ github-option-three-app/src/app/components/about/about.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+import { FieldsetModule } from 'primeng/fieldset';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FieldsetModule, AvatarModule,AvatarGroupModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  mcl = "{{ MonaCodeLisa }}"
 
+  links = [
+		{
+			icon: "pi pi-globe",
+			url: "https://monacodelisa.com",
+		},
+		{
+			icon: "pi pi-github",
+			url: "https://github.com/monacodelisa",
+		},
+		{
+			icon: "pi pi-linkedin",
+			url: "https://www.linkedin.com/in/monacodelisa/",
+		},
+	];
 }
