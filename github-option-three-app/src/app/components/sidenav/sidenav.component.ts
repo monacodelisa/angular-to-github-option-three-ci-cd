de1e62d22f52d46d4c84d530b1149ab4dbdb9bd9
Esther White <monacodelisa@gmail.com>
2024-02-07 23:50:00 +0200

    for now

--- github-option-three-app/src/app/components/sidenav/sidenav.component.ts
+++ github-option-three-app/src/app/components/sidenav/sidenav.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet, RouterLink, RouterLinkActive } from "@angular/router";
 import { ToolbarModule } from "primeng/toolbar";
 
 @Component({
 	selector: "app-sidenav",
 	standalone: true,
-	imports: [RouterOutlet, RouterLink, RouterLinkActive, ToolbarModule],
+	imports: [FormsModule, ReactiveFormsModule, RouterOutlet, RouterLink, RouterLinkActive, ToolbarModule],
 	templateUrl: "./sidenav.component.html",
 	styleUrl: "./sidenav.component.css",
 })
 export class SidenavComponent {
 	@Input() sidenavOpen: boolean = true;
+
+  date: Date[] | undefined;
 
 	items = [
 		{
@@ -27,6 +30,6 @@ export class SidenavComponent {
 			label: "About Angular",
 			icon: "pi pi-shield",
 			route: "/about-angular",
-		},
+		}
 	];
 }
